21d396e3b232c83b1281515f15943e5a8b274e83
Sathya Iyer <sathya@Sathyas-Air.localdomain>
2023-07-29 19:30:31 -0700

    Update all-in podcast with new episode E139

--- main.py
+++ main.py
@@ -3,6 +3,7 @@ from langchain import PromptTemplate
 from langchain.llms import OpenAI
 import streamlit.components.v1 as components
 import json
+import os
 
 
 def convert(seconds):
@@ -66,100 +67,136 @@ def chapter_content_function(vid_id, chapters, chap_text):
     st.write(chap_description)
 
 
-def get_qa_contents(vid_id):
-    with open(f"data/{video_id}_qa.json", "r") as fh:
+def get_qa_contents(output_path, video_id):
+    with open(f"{output_path}/{video_id}/qa.json", "r") as fh:
         return json.load(fh)
 
 
-def show_qa(vid_id, qa_contents):
+def show_qa(output_path, video_id):
     st.markdown("Curated Question Answers from the video")
-    qa = qa_contents["qa"]
-    first = True
-    q_titles = []
+    with open(f"{output_path}/{video_id}/qa.json", "r") as fh:
+        qa = json.load(fh)
+        first = True
+        q_titles = []
 
-    for i, qa_info in enumerate(qa):
-        question = qa_info["question"]
-        q_titles.append(f"{i+1}. {question}")
+        for i, qa_info in enumerate(qa):
+            question = qa_info["question"]
+            q_titles.append(f"{i+1}. {question}")
 
-    question_sel = st.selectbox("", q_titles)
-    qa_content_function(vid_id, qa, question_sel)
+        question_sel = st.selectbox("", q_titles)
+        qa_content_function(video_id, qa, question_sel)
 
 
-def show_chapters(vid_id, video_contents):
+def show_chapters(
+    output_path,
+    video_id,
+):
     st.markdown("### Chapter Summary")
-    chapters = video_contents["chapter_summary"]
-    first = True
-    chap_titles = []
+    with open(f"{output_path}/{video_id}/chapter_summary.json", "r") as fh:
+        chapters = json.load(fh)
+        first = True
+        chap_titles = []
 
-    for i, chapter in enumerate(chapters):
-        chap_title = chapter["title"]
-        vid_url = get_video_url(vid_id, chapter["start"])
-        chap_start_time = convert(chapter["start"])
-        chap_titles.append(f"{i+1}. ({chap_start_time}) {chap_title} ")
+        for i, chapter in enumerate(chapters):
+            chap_title = chapter["title"]
+            vid_url = get_video_url(video_id, chapter["start"])
+            chap_start_time = convert(chapter["start"])
+            chap_titles.append(f"{i+1}. ({chap_start_time}) {chap_title} ")
 
-    chapter_sel = st.selectbox("", chap_titles)
+        chapter_sel = st.selectbox("", chap_titles)
 
-    chapter_content_function(vid_id, chapters, chapter_sel)
+        chapter_content_function(video_id, chapters, chapter_sel)
 
 
-def show_speakers(video_contents):
+def show_speakers(output_path, video_id):
     st.markdown("## Speaker Summary")
-    speakers = video_contents["speaker_summary"]
-    sps = []
-    for sp in speakers:
-        sps.append(sp)
-    s_tabs = st.tabs(sps)
-    for i, s_tab in enumerate(s_tabs):
-        s_tab.write("".join(speakers[sps[i]]))
-    st.divider()
+    with open(f"{output_path}/{video_id}/speaker_summary.json", "r") as fh:
+        speakers = json.load(fh)
+        sps = []
+        for sp in speakers:
+            sps.append(sp)
+        s_tabs = st.tabs(sps)
+        for i, s_tab in enumerate(s_tabs):
+            s_tab.write("".join(speakers[sps[i]]))
+        st.divider()
 
 
-def get_video_url(vid_id, secs):
+def get_video_url(video_id, secs):
     return f"https://www.youtube.com/watch?v={video_id}&t={secs}"
 
 
 title = "All-In Podcast GPT"
 channel = "All-IN Podcast"
-video_id = "mpC6c6iYji8"
-video_url = get_video_url(video_id, 0)
-video_title = (
-    "E138: Presidential Candidate Vivek Ramaswamy in conversation with the Besties"
-)
+all_in_directory = f"data/all_in"
+
+dir_list = os.listdir(all_in_directory)
+
+video_summary_info = {}
+video_titles = []
+for video_id_path in dir_list:
+    with open(f"{all_in_directory}/{video_id_path}/video_summary.json", "r") as fh:
+        video_summary = json.load(fh)
+        video_title = video_summary["title"]
+        video_summary_info[video_title] = {
+            "video_id": video_id_path,
+            "summary": video_summary["summary"],
+        }
+        video_titles.append(video_title)
 
 st.set_page_config(page_title=title, page_icon=":robot:")
 st.header(title)
 st.markdown(
-    f" This page contains GPT generated output for {channel}  **{video_title}** "
-)
-st.divider()
-
-# video_display = get_youtube_window(video_id)
-col1, col2 = st.columns([3, 2])
-
-with col1:
-    st.markdown("### Video Summary")
-    video_contents = get_contents(video_id=video_id)
-    st.markdown("".join(video_contents["summary"]))
-    st.divider()
-    show_speakers(video_contents=video_contents)
-    st.divider()
-    show_chapters(video_id, video_contents=video_contents)
-    # qa_contents = get_qa_contents(video_id)
-    # st.divider()
-    # show_qa(video_id,qa_contents)
-
-with col2:
-    st.video(video_url)
-    st.markdown(video_title)
-    st.divider()
-
-st.divider()
-
-st.markdown("## Curated Question Answers from the video")
-st.markdown(
-    "<small>I will add support for dynamic question and answering in next video",
+    f" This page contains GPT generated output for {channel} ",
     unsafe_allow_html=True,
 )
-qa_contents = get_qa_contents(video_id)
-for i, qa in enumerate(qa_contents["qa"]):
-    display_qa_content(video_id, qa, i)
+
+
+def display_video_content(video_title):
+    video_info = video_summary_info[video_title]
+    video_id = video_info["video_id"]
+
+    # video_display = get_youtube_window(video_id)
+    col1, col2 = st.columns([3, 2])
+
+    with col1:
+        st.markdown("### Video Summary")
+        st.markdown("".join(video_info["summary"]))
+        st.divider()
+        show_speakers(
+            all_in_directory,
+            video_id,
+        )
+        st.divider()
+        show_chapters(
+            all_in_directory,
+            video_id,
+        )
+
+        st.divider()
+        show_qa(all_in_directory, video_id)
+
+    with col2:
+        video_url = get_video_url(video_id, 0)
+        st.video(video_url)
+        st.markdown(video_title)
+        st.divider()
+
+    st.divider()
+
+    st.markdown("## Curated Question Answers from the video")
+    st.markdown(
+        "<small>I will add support for dynamic question and answering in next video",
+        unsafe_allow_html=True,
+    )
+    qa_contents = get_qa_contents(all_in_directory, video_id)
+    for i, qa in enumerate(qa_contents):
+        display_qa_content(video_id, qa, i)
+
+
+episodes_selection = st.selectbox(
+    "Select the episode for generating video details!!", video_titles
+)
+st.divider()
+display_video_content(episodes_selection)
+
+st.divider()
